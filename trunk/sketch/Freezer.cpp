b20e133f2ed7457bba09f54c0d4b32a021d0b578
g.remigi@gmail.com <g.remigi@gmail.com@52b96aab-37a1-e541-e895-d5b4e8dfd823>
2011-07-10 22:23:28 +0000

    Arduino sketch first draft.

---
+++ trunk/sketch/Freezer.cpp
@@ -0,0 +1,72 @@
+#include "Freezer.h"
+   
+/** Default constructor. */
+Freezer::Freezer() {
+}
+ 
+/** Desctructor. */
+Freezer::~Freezer(){
+}
+
+/** Circuit initialization. */
+void Freezer::initialize() {
+  
+    // Set pins to output so you can control the shift register.
+    pinMode(LATCH_PIN, OUTPUT);
+    pinMode(CLOCK_PIN, OUTPUT);
+    pinMode(DATA_PIN, OUTPUT);
+    pinMode(OUTPUT_ENABLED_PIN, OUTPUT);
+    pinMode(OVERRIDING_CLEAR_PIN, OUTPUT);
+    
+    // Set pins to input so you can trigger the circuit.
+    pinMode(INPUT1_PIN , INPUT);
+    pinMode(INPUT2_PIN , INPUT);
+    
+    // Set the pins values.
+    digitalWrite(LATCH_PIN, LOW);
+    digitalWrite(CLOCK_PIN, LOW);
+    digitalWrite(DATA_PIN, LOW);
+    digitalWrite(OUTPUT_ENABLED_PIN, LOW);
+    digitalWrite(OVERRIDING_CLEAR_PIN, HIGH);
+}
+
+/**
+ * Write the given string, a sequence of 0 and 1, into the shift registers.
+ * @param flags The string representing a 
+ *     sequence of bits.
+ */
+void Freezer::write(unsigned int value) {
+
+    unsigned int mask = 1u;
+  
+    // Clear everything to prepare shift register for bit shifting.
+    digitalWrite(DATA_PIN, LOW);
+    digitalWrite(CLOCK_PIN, LOW);
+    digitalWrite(LATCH_PIN, LOW);
+
+    for (int i = 0; i < 16; i++) {
+      
+        // Reset the clock pin.
+        digitalWrite(CLOCK_PIN, LOW);
+
+        // Write a bit into the registers.
+        if ((value & mask) == mask) {
+            digitalWrite(DATA_PIN, HIGH);
+        } else {
+            digitalWrite(DATA_PIN, LOW);
+        }
+  
+        // Register shifts bits on upstroke of clock pin. 
+        digitalWrite(CLOCK_PIN, HIGH);
+        
+        // Zero the data pin after shift to prevent bleed through.
+        digitalWrite(DATA_PIN, LOW);
+        
+        mask <<= 1;
+    }
+
+    // Stop shifting.
+    digitalWrite(CLOCK_PIN, LOW);
+    digitalWrite(LATCH_PIN, HIGH);
+}
+
---
+++ trunk/sketch/Freezer.h
@@ -0,0 +1,43 @@
+#ifndef FREEZER_H
+#define FREEZER_H
+
+#include <WProgram.h>
+
+/**
+ * This is the main class to control Freezer.
+ * A write method with a 16 bits value allows the user to control the
+ * 16 outputs if the shift registers.
+ */
+class Freezer {
+
+public:
+
+    // Pin D6 connected to ST_CP of 74HC595.
+    static const byte LATCH_PIN = 6;
+    
+    // Pin D3 connected to SH_CP of 74HC595.
+    static const byte CLOCK_PIN = 3;
+    
+    // Pin D4 connected to DS of 74HC595.
+    static const byte DATA_PIN = 4;
+    
+    // Pin D5 connected to OE of 74HC595. Low active.
+    static const byte OUTPUT_ENABLED_PIN = 5;
+    
+    // Pin D2 connected to MR of of 74HC595. Low active.
+    static const byte OVERRIDING_CLEAR_PIN = 2;
+    
+    // Pin D7 in read mode.
+    static const byte INPUT1_PIN = 7;
+    
+    // Pin D8 in read mode.
+    static const byte INPUT2_PIN = 8;
+    
+    Freezer();
+    ~Freezer();
+    void initialize();
+    void write(unsigned int value);
+};
+
+#endif
+
